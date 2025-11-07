c2fa08bcee23ad814c7b98818c927ae602b33f73
marvellous <marvellousoseh3@gmail.com>
2025-11-07 08:41:49 +0100

    jdjd

---
+++ php-vercel-ig/api/readme.txt
@@ -0,0 +1,5 @@
+change connect.php on instagram folder to your correct sql
+
+
+
+import flora_support(1) into your sql database 
---
+++ php-vercel-ig/instagram/connect.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $conn = mysqli_connect('localhost', 'root', '', 'flora_support');
+
+         if (!$conn) {
+                  die('error');
+                       }else {
+                               //  echo "we are connected";
+                                    }
+
+
+
+
+                                    ?>
